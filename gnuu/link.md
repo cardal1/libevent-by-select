27aeb1ff400de121d19c2ba70d3de1dd45d25354
benpeng.jiang <benpeng.jiang@ttpod.com>
2015-05-14 20:18:07 +0800

    add codes for gnuu

--- gnuu/link.md
+++ gnuu/link.md
@@ -1,1 +1,1 @@
-http://gnuu.org/2009/09/18/writing-your-own-toy-compiler/all/1/
+https://github.com/lsegal/my_toy_compiler
--- gnuu/main.cpp
+++ gnuu/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include "codegen.h"
 #include "node.h"
-extern NBlock* programBlock;
+
+using namespace std;
+
 extern int yyparse();
+extern NBlock* programBlock;
+
+void createCoreFunctions(CodeGenContext& context);
 
 int main(int argc, char **argv)
 {
-    yyparse();
-    std::cout << programBlock << std::endl;
-    return 0;
+	yyparse();
+	cout << programBlock << endl;
+    // see http://comments.gmane.org/gmane.comp.compilers.llvm.devel/33877
+	InitializeNativeTarget();
+	CodeGenContext context;
+	createCoreFunctions(context);
+	context.generateCode(*programBlock);
+	context.runCode();
+	
+	return 0;
 }
+
